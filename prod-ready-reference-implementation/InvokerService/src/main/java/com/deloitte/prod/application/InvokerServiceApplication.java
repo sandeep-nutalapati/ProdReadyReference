f069718e5212ae0e45bc24058a669aa4afced2c9
deepsahoo <deepsahoo@USBLRDEEPSAH009.us.deloitte.com>
2020-06-27 18:26:25 +0530

    rest template

--- prod-ready-reference-implementation/InvokerService/src/main/java/com/deloitte/prod/application/InvokerServiceApplication.java
+++ prod-ready-reference-implementation/InvokerService/src/main/java/com/deloitte/prod/application/InvokerServiceApplication.java
@@ -3,7 +3,10 @@ package com.deloitte.prod.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.deloitte.prod.*")
@@ -12,4 +15,9 @@ public class InvokerServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InvokerServiceApplication.class, args);
 	}
+	
+	@Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
--- prod-ready-reference-implementation/InvokerService/src/main/java/com/deloitte/prod/controller/InvokerController.java
+++ prod-ready-reference-implementation/InvokerService/src/main/java/com/deloitte/prod/controller/InvokerController.java
@@ -3,9 +3,14 @@ package com.deloitte.prod.controller;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.deloitte.prod.model.ProdDetail;
 
 @RestController
 @RequestMapping("/invoked-service")
@@ -13,10 +18,21 @@ public class InvokerController {
 	
 	private static final Logger LOG = Logger.getLogger(InvokerController.class.getName());
 	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	
     @RequestMapping(value = "/intializeInvokerService", method= RequestMethod.GET)
 	public String intializeInvokerService() {
 		LOG.log(Level.INFO, "Invoker controller is being called");
 		return "success";
+	}
+    
+    @RequestMapping(value = "/saveServiceDetails", method = RequestMethod.POST)
+	public String saveServiceDetails(@RequestBody ProdDetail detaills) {
+		LOG.log(Level.INFO, "saveServiceDetails is being called");
+		String response = restTemplate.postForObject("http://localhost:8090/invoked-service/addServiceDetails", detaills, String.class);
+		return response;
 	}
 
 }
